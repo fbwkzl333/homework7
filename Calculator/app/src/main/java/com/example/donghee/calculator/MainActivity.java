a5e1e04532c75f2cef60561396038743592f20ac
fbwkzl333 <fbwkzl111@naver.com>
2016-11-16 19:03:32 +0900

    [더하기] 버튼 기능 완료

--- Calculator/app/src/main/java/com/example/donghee/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/donghee/calculator/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.donghee.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView resultView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button btn1 = (Button) findViewById(R.id.button1);
+        resultView = (TextView) findViewById(R.id.textView);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText edit1 = (EditText) findViewById(R.id.editText1);
+                EditText edit2 = (EditText) findViewById(R.id.editText2);
+                int num1 = Integer.parseInt(edit1.getText().toString());
+                int num2 = Integer.parseInt(edit2.getText().toString());
+                int result = num1 + num2;
+                resultView.setText("계산 결과 : "+result);
+            }
+        });
     }
 }
