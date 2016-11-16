a07b2095e708ee0eda63593461d82b00a0299cb8
fbwkzl333 <fbwkzl111@naver.com>
2016-11-16 22:12:17 +0900

    EditText가 없을시 입력해달라고 토스트메시지를 남김

--- Calculator/app/src/main/java/com/example/donghee/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/donghee/calculator/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.donghee.calculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,10 +28,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText edit1 = (EditText) findViewById(R.id.editText1);
                 EditText edit2 = (EditText) findViewById(R.id.editText2);
-                int num1 = Integer.parseInt(edit1.getText().toString());
-                int num2 = Integer.parseInt(edit2.getText().toString());
-                int result = num1 + num2;
-                resultView.setText("계산 결과 : "+result);
+
+                String num1 = edit1.getText().toString();
+                String num2 = edit2.getText().toString();
+                if(num1.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"첫번째 수를 입력하세요. ", Toast.LENGTH_SHORT).show();
+                }
+                else if(num2.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"두번째 수를 입력하세요. ", Toast.LENGTH_SHORT).show();
+                }else {
+                    int result = Integer.parseInt(num1) + Integer.parseInt(num2);
+                    resultView.setText("계산 결과 : " + result);
+                }
             }
         });
 
@@ -39,10 +48,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText edit1 = (EditText) findViewById(R.id.editText1);
                 EditText edit2 = (EditText) findViewById(R.id.editText2);
-                int num1 = Integer.parseInt(edit1.getText().toString());
-                int num2 = Integer.parseInt(edit2.getText().toString());
-                int result = num1 - num2;
-                resultView.setText("계산 결과 : "+result);
+
+                String num1 = edit1.getText().toString();
+                String num2 = edit2.getText().toString();
+                if(num1.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"첫번째 수를 입력하세요. ", Toast.LENGTH_SHORT).show();
+                }
+                else if(num2.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"두번째 수를 입력하세요. ", Toast.LENGTH_SHORT).show();
+                }else {
+                    int result = Integer.parseInt(num1) - Integer.parseInt(num2);
+                    resultView.setText("계산 결과 : " + result);
+                }
             }
         });
 
@@ -51,22 +68,38 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText edit1 = (EditText) findViewById(R.id.editText1);
                 EditText edit2 = (EditText) findViewById(R.id.editText2);
-                int num1 = Integer.parseInt(edit1.getText().toString());
-                int num2 = Integer.parseInt(edit2.getText().toString());
-                int result = num1 * num2;
-                resultView.setText("계산 결과 : "+result);
+
+                String num1 = edit1.getText().toString();
+                String num2 = edit2.getText().toString();
+                if(num1.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"첫번째 수를 입력하세요. ", Toast.LENGTH_SHORT).show();
+                }
+                else if(num2.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"두번째 수를 입력하세요. ", Toast.LENGTH_SHORT).show();
+                }else {
+                    int result = Integer.parseInt(num1) * Integer.parseInt(num2);
+                    resultView.setText("계산 결과 : " + result);
+                }
             }
         });
 
-        btn4.setOnClickListener(new View.OnClickListener() {
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText edit1 = (EditText) findViewById(R.id.editText1);
                 EditText edit2 = (EditText) findViewById(R.id.editText2);
-                int num1 = Integer.parseInt(edit1.getText().toString());
-                int num2 = Integer.parseInt(edit2.getText().toString());
-                int result = num1 / num2;
-                resultView.setText("계산 결과 : "+result);
+
+                String num1 = edit1.getText().toString();
+                String num2 = edit2.getText().toString();
+                if(num1.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"첫번째 수를 입력하세요. ", Toast.LENGTH_SHORT).show();
+                }
+                else if(num2.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"두번째 수를 입력하세요. ", Toast.LENGTH_SHORT).show();
+                }else {
+                    int result = Integer.parseInt(num1) / Integer.parseInt(num2);
+                    resultView.setText("계산 결과 : " + result);
+                }
             }
         });
     }
