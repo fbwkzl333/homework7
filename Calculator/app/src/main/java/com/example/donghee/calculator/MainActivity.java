aded91b114260ac8d0600bd52d92918863e52f4f
fbwkzl333 <fbwkzl111@naver.com>
2016-11-18 10:43:39 +0900

    나누기, 실수형, 0으로 못나눔, 타이틀 수정 update

--- Calculator/app/src/main/java/com/example/donghee/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/donghee/calculator/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import static java.lang.Double.parseDouble;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView resultView;
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         Button btn2 = (Button) findViewById(R.id.button2);
         Button btn3 = (Button) findViewById(R.id.button3);
         Button btn4 = (Button) findViewById(R.id.button4);
+        Button btn5 = (Button) findViewById(R.id.button5);
         resultView = (TextView) findViewById(R.id.textView);
 
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -37,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
                 else if(num2.isEmpty()){
                     Toast.makeText(getApplicationContext(),"두번째 수를 입력하세요. ", Toast.LENGTH_SHORT).show();
                 }else {
-                    int result = Integer.parseInt(num1) + Integer.parseInt(num2);
+                    Double result = parseDouble(num1) + parseDouble(num2);
                     resultView.setText("계산 결과 : " + result);
                 }
             }
@@ -57,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
                 else if(num2.isEmpty()){
                     Toast.makeText(getApplicationContext(),"두번째 수를 입력하세요. ", Toast.LENGTH_SHORT).show();
                 }else {
-                    int result = Integer.parseInt(num1) - Integer.parseInt(num2);
+                    Double result = parseDouble(num1) - parseDouble(num2);
                     resultView.setText("계산 결과 : " + result);
                 }
             }
@@ -77,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
                 else if(num2.isEmpty()){
                     Toast.makeText(getApplicationContext(),"두번째 수를 입력하세요. ", Toast.LENGTH_SHORT).show();
                 }else {
-                    int result = Integer.parseInt(num1) * Integer.parseInt(num2);
+                    Double result = parseDouble(num1) * parseDouble(num2);
                     resultView.setText("계산 결과 : " + result);
                 }
             }
@@ -96,8 +99,34 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else if(num2.isEmpty()){
                     Toast.makeText(getApplicationContext(),"두번째 수를 입력하세요. ", Toast.LENGTH_SHORT).show();
-                }else {
-                    int result = Integer.parseInt(num1) / Integer.parseInt(num2);
+                }else if(parseDouble(num1) == 0 || parseDouble(num2) == 0) {
+                    Toast.makeText(getApplicationContext(),"0으로 나눌 수 없습니다.",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Double result = parseDouble(num1) / parseDouble(num2);
+                    resultView.setText("계산 결과 : " + result);
+                }
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText edit1 = (EditText) findViewById(R.id.editText1);
+                EditText edit2 = (EditText) findViewById(R.id.editText2);
+
+                String num1 = edit1.getText().toString();
+                String num2 = edit2.getText().toString();
+                if(num1.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"첫번째 수를 입력하세요. ", Toast.LENGTH_SHORT).show();
+                }
+                else if(num2.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"두번째 수를 입력하세요. ", Toast.LENGTH_SHORT).show();
+                }else if(parseDouble(num1) == 0 || parseDouble(num2) == 0) {
+                    Toast.makeText(getApplicationContext(),"0으로 나눌 수 없습니다.",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Double result = parseDouble(num1) % parseDouble(num2);
                     resultView.setText("계산 결과 : " + result);
                 }
             }
